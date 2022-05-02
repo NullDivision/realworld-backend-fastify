d70851cfe8364ae02363f5ee8a763259e74a28c4
NullDivision <penumbra.scilicet@gmail.com>
2022-05-03 01:25:13 +0300

    fix: make authentication optional for profile route

--- src/routers/profiles.ts
+++ src/routers/profiles.ts
@@ -1,7 +1,7 @@
 import type { FastifyPluginCallback } from 'fastify';
 import { User, getUserDb, getFollowersDb } from '../data';
 
-type TokenizedUser = Omit<User, 'password' | 'token' | 'user_id'>;
+type TokenizedUser = Omit<User, 'password' | 'token' | 'user_id' | 'email'>;
 
 interface ProfileGeneric {
   Params: { username: string };
@@ -9,8 +9,6 @@ interface ProfileGeneric {
 }
 
 export const router: FastifyPluginCallback = (instance, options, done) => {
-  instance.addHook('onRequest', instance.authenticate);
-
   instance.get<ProfileGeneric>('/:username', async (request, reply) => {
     const [currentUser, followedUser] = await Promise.all([
       getUserDb()
@@ -18,69 +16,75 @@ export const router: FastifyPluginCallback = (instance, options, done) => {
         .where('token', request.headers.authorization?.replace('Bearer ', ''))
         .first(),
       getUserDb()
-        .select('bio', 'email', 'image', 'user_id', 'username')
+        .select('bio', 'image', 'user_id', 'username')
         .where('username', request.params.username)
         .first()
     ]);
 
-    if (!followedUser || !currentUser) return reply.send({ profile: null });
+    if (!followedUser) return reply.send({ profile: null });
 
     const { user_id: followedUserId, ...restFollowedUser } = followedUser;
-    const followState = await getFollowersDb()
+    const followState = currentUser ? await getFollowersDb()
       .where('following_id', followedUserId)
       .andWhere('user_id', currentUser.user_id)
-      .first();
+      .first() : false;
 
     return reply.send({
       profile: { ...restFollowedUser, following: !!followState }
     });
   });
 
-  instance.post<ProfileGeneric>('/:username/follow', async (request, reply) => {
-    const [currentUser, followedUser] = await Promise.all([
-      getUserDb()
-        .select('user_id')
-        .where('token', request.headers.authorization?.replace('Bearer ', ''))
-        .first(),
-      getUserDb()
-        .select('bio', 'email', 'image', 'user_id', 'username')
-        .where('username', request.params.username)
-        .first()
-    ]);
+  instance.post<ProfileGeneric>('/:username/follow', {
+    handler: async (request, reply) => {
+      const [currentUser, followedUser] = await Promise.all([
+        getUserDb()
+          .select('user_id')
+          .where('token', request.headers.authorization?.replace('Bearer ', ''))
+          .first(),
+        getUserDb()
+          .select('bio', 'image', 'user_id', 'username')
+          .where('username', request.params.username)
+          .first()
+      ]);
 
-    if (!followedUser || !currentUser) return reply.send({ profile: null });
+      if (!followedUser || !currentUser) return reply.send({ profile: null });
 
-    await getFollowersDb().insert({
-      following_id: followedUser.user_id,
-      user_id: currentUser.user_id
-    });
+      await getFollowersDb().insert({
+        following_id: followedUser.user_id,
+        user_id: currentUser.user_id
+      });
 
-    const { user_id, ...restFollowedUser } = followedUser;
+      const { user_id, ...restFollowedUser } = followedUser;
 
-    await reply.send({ profile: { ...restFollowedUser, following: true } });
+      await reply.send({ profile: { ...restFollowedUser, following: true } });
+    },
+    onRequest: [instance.authenticate]
   });
 
-  instance.delete<ProfileGeneric>('/:username/follow', async (request, reply) => {
-    const [currentUser, followedUser] = await Promise.all([
-      getUserDb()
-        .select('user_id')
-        .where('token', request.headers.authorization?.replace('Bearer ', ''))
-        .first(),
-      getUserDb()
-        .select('bio', 'email', 'image', 'user_id', 'username')
-        .where('username', request.params.username)
-        .first()
-    ]);
+  instance.delete<ProfileGeneric>('/:username/follow', {
+    handler: async (request, reply) => {
+      const [currentUser, followedUser] = await Promise.all([
+        getUserDb()
+          .select('user_id')
+          .where('token', request.headers.authorization?.replace('Bearer ', ''))
+          .first(),
+        getUserDb()
+          .select('bio', 'image', 'user_id', 'username')
+          .where('username', request.params.username)
+          .first()
+      ]);
 
-    if (!followedUser || !currentUser) return reply.send({ profile: null });
+      if (!followedUser || !currentUser) return reply.send({ profile: null });
 
-    const { user_id, ...restFollowedUser } = followedUser;
+      const { user_id, ...restFollowedUser } = followedUser;
 
-    await getFollowersDb()
-      .where('following_id', user_id).andWhere('user_id', currentUser.user_id)
-      .delete();
+      await getFollowersDb()
+        .where('following_id', user_id).andWhere('user_id', currentUser.user_id)
+        .delete();
 
-    await reply.send({ profile: { ...restFollowedUser, following: false } });
+      await reply.send({ profile: { ...restFollowedUser, following: false } });
+    },
+    onRequest: [instance.authenticate]
   });
 
   done();
